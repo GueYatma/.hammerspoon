873465d96d00fcee82edd007f869b5e5491a701d
GueYatma <gueye.yatma@gmail.com>
2026-01-31 14:27:15 +0100

    Sauvegarde Rapide

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ require("modules.shortcuts")
 -- 4. Gestion des Bureaux & GPS
 require("modules.desktops")
 
--- 5. Dashboard (Désactivé pour l'instant) modifs pour gth
+-- 5. Dashboard (Désactivé pour l'instant)
 require("modules.dashboard")
 
 -- 6. Intelligence Artificielle (Le fameux ai_paste)
